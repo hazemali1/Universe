e6e5d893c485579712c8fb6a129a24bd42309eb3
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-18 21:03:19 +0200

    classes

--- run.py
+++ run.py
@@ -3,30 +3,30 @@ from galaxies import Galaxie
 from solar_systems import Solar_System
 from planets import Planet
 from details import Detail
-from db import session
+from db import db
 
 
 
 obj1 = Galaxie("Galaxie1")
-session.add(obj1)
-session.commit()
+db.add(obj1)
+db.commit()
 
 obj2 = Solar_System("Solar_System1", galaxie_id=obj1.id)
-session.add(obj2)
-session.commit()
+db.add(obj2)
+db.commit()
 
 obj3 = Planet("Planets1", solar_system_id=obj2.id)
-session.add(obj3)
-session.commit()
+db.add(obj3)
+db.commit()
 
 obj4 = Detail("Details for galaxy", details_Galaxies_id=obj1.id)
-session.add(obj4)
-session.commit()
+db.add(obj4)
+db.commit()
 
 obj5 = Detail("Details for solar system", details_Solar_Systems_id=obj2.id)
-session.add(obj5)
-session.commit()
+db.add(obj5)
+db.commit()
 
 obj6 = Detail("Details for planet", details_Planets_id=obj3.id)
-session.add(obj6)
-session.commit()
+db.add(obj6)
+db.commit()

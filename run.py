b54bb89f4a20bb0949411bca54fae49dbc969d4e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-18 19:48:41 +0200

    classes

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from UNIVERSE.galaxies import Galaxie
-from UNIVERSE.solar_systems import Solar_System
-from UNIVERSE.planets import Planets
-from UNIVERSE.db import session
+from galaxies import Galaxie
+from solar_systems import Solar_System
+from planets import Planets
+from db import session
 
 
 

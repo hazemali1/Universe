c4e61efcccc59c0a294859b9071a2f05774b896d
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-20 13:22:49 +0200

    classes

--- run.py
+++ run.py
@@ -14,27 +14,13 @@ Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 db = Session()
 
+in_put = ""
+comands = {
+	"for new galaxy": "new galaxy",
+	"for new solar system": "new solar system",
+	"for new planet": "new planet",
+	"if tyou wanna exit": "Exit"
+}
 
-obj1 = Galaxie("Galaxie1")
-db.add(obj1)
-db.commit()
-
-obj2 = Solar_System("Solar_System1", galaxie_id=obj1.id)
-db.add(obj2)
-db.commit()
-
-obj3 = Planet("Planets1", solar_system_id=obj2.id)
-db.add(obj3)
-db.commit()
-
-obj4 = Detail("Details for galaxy", details_Galaxies_id=obj1.id)
-db.add(obj4)
-db.commit()
-
-obj5 = Detail("Details for solar system", details_Solar_Systems_id=obj2.id)
-db.add(obj5)
-db.commit()
-
-obj6 = Detail("Details for planet", details_Planets_id=obj3.id)
-db.add(obj6)
-db.commit()
+while (in_put != "Exit"):
+	in_put = input(comands)
